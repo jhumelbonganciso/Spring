1a5dc5f289090526478f384b804ab6b075c4be01
jhumelbonganciso <jhumelbonganciso04@gmail.com>
2023-05-18 23:15:11 +0800

    Wiring beans using method parameters

---
+++ WiringBeans/src/main/java/org/spring/throughMethodParameter/main/Main.java
@@ -0,0 +1,26 @@
+package org.spring.throughMethodParameter.main;
+
+import org.spring.throughMethodCall.bean.Person;
+import org.spring.throughMethodCall.bean.Vehicle;
+import org.spring.throughMethodCall.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        * Without specifying the correct bean, if you have a bean with same data type
+        * it will throw NoUniqueBeanDefinitionException.
+        * So you need to throw what bean are you pertaining to.
+        * */
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var person = context.getBean(Person.class);
+        var veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+
+        System.out.println("Person from bean " + person.getName());
+        System.out.println("Vehicle that person own is: " + person.getVehicle());
+
+
+    }
+}
